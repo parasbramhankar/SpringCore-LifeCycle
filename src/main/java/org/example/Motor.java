766eca3e85bb4132aadb1ef834cce5096359f038
parasbramhankar <parasbramhankar.in@gmail.com>
2025-11-22 13:46:07 +0530

    Commit

---
+++ src/main/java/org/example/Motor.java
@@ -0,0 +1,18 @@
+package org.example;
+
+public class Motor {
+
+    void start(){
+        System.out.println("Motor Started");
+    }
+    void stop(){
+        System.out.println("Motor stopped");
+    }
+    void doWork(){
+        System.out.println("Motor is working");
+    }
+    void doServicing(){
+        System.out.println("Motor is in the servicing mode");
+    }
+
+}
